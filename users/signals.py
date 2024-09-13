031d28871b82f20e23bf7d729230770e36da3ee8
Athul <athulkrishna9103@gmail.com>
2024-09-13 18:22:43 +0530

    commit

--- users/signals.py
+++ users/signals.py
@@ -2,13 +2,23 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
+import logging
 
+logger = logging.getLogger(__name__)
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
+        logger.info('Creating profile instance for user %s', instance.username)
         Profile.objects.create(user=instance)
-
+        logger.info('Profile instance created successfully')
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, **kwargs):
     instance.profile.save()
+
+
+# new modified code
+
+
+
+
